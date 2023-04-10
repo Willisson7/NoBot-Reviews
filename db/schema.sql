d203dd13605adb8009737b996d2ed8a720d44e02
JosieSavill <fiordasavilljosie@gmail.com>
2023-04-10 10:31:16 -0700

    changes to controllers/api/userRoutes.js, schema, seeds, models/REview.js, server.js, readme

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,18 @@
 DROP DATABASE IF EXISTS nobots_db;
 CREATE DATABASE nobots_db;
+
+USE nobots_db;
+
+CREATE TABLE User(
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Review(
+  id INT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  comment_info TEXT NOT NULL,
+  user_id INT NOT NULL
+);
