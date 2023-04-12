8b914f30354f3c54c89fc15d3f706acea184afd6
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-04-11 18:47:42 -0700

    Added new handlebars and worked to create signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,4 +84,17 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// This will handle directing to a signup.handlebars
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (!req.session.logged_in) {
+    res.render('signup.handlebars');
+    // res.redirect('/profile');
+     return;
+  }
+
+  res.redirect('/');
+});
+
+
 module.exports = router;
